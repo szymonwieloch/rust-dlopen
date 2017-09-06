8c6b53e4bd06ecf3ca32b49a07195babdf35a468
truenagual <true.nagual@gmail.com>
2017-09-06 22:15:30 +0200

    Fixed handling error from creating error mode guard

--- src/raw/windows.rs
+++ src/raw/windows.rs
@@ -129,7 +129,7 @@ pub unsafe fn get_sym(handle: Handle, name: &CStr) -> Result<*mut (), Error> {
 #[inline]
 pub unsafe fn open_lib(name: &OsStr) -> Result<Handle, Error> {
     let wide_name: Vec<u16> = name.encode_wide().chain(Some(0)).collect();
-    let _guard = ErrorModeGuard::new();
+    let _guard = ErrorModeGuard::new()?;
     let handle = kernel32::LoadLibraryW(wide_name.as_ptr());
     if handle.is_null() {
         Err(Error::OpeningLibraryError(get_win_error()))
